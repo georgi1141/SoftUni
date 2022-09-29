bcab42edde2f483ac8207fc9fa23c8a78cce475a
georgi1141 <g.zh.yanev@gmail.com>
2022-09-29 23:51:05 +0200

    update

--- JS Fundamentals-September 2022/5.Arrays/Arrays-Ecercise/5.maxNumber.js
+++ JS Fundamentals-September 2022/5.Arrays/Arrays-Ecercise/5.maxNumber.js
@@ -24,3 +24,17 @@ function maxNum(nums) {
 }
 
 maxNum([41, 41, 34, 20]);
+
+/*
+                            Easy solution from class-mate
+function maxNumber(arr) {
+    let result = [];
+    while (arr.length !== 0) {
+        let current = arr.shift(); // take the first number from array
+        let arrMax = Math.max(...arr); // find biggest number in array left
+        current > arrMax ? result.push(current) : null; // compared one to each other
+    }
+    console.log(result.join(' '));
+}
+maxNumber([13, 45, 48])
+*/
