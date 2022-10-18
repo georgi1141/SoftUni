e9f49796ce2d70f94dbc04fb4a4df52892f5d59a
georgi1141 <g.zh.yanev@gmail.com>
2022-10-18 16:01:06 +0200

    update

---
+++ JS Fundamentals-September 2022/8.Exam Preparation/1.blackFlag.js
@@ -0,0 +1,28 @@
+function plunderColection(arr) {
+  let days = Number(arr.shift());
+  let plunderPerDay = Number(arr.shift());
+  let expecterPlunder = Number(arr.shift());
+
+  let totalPlunder = 0;
+  // iterate the number of days
+  for (let i = 1; i <= days; i++) {
+    // add dayli plunder to the total plunder
+    totalPlunder += plunderPerDay;
+
+    if (i % 3 === 0) {
+      totalPlunder += plunderPerDay * 0.5;
+    }
+    if (i % 5 === 0) {
+      totalPlunder *= 0.7;
+    }
+  }
+  totalPlunder >= expecterPlunder
+    ? console.log(`Ahoy! ${totalPlunder.toFixed(2)} plunder gained.`)
+    : console.log(
+      `Collected only ${((totalPlunder / expecterPlunder) * 100).toFixed(
+        2
+      )}% of the plunder.`
+    );
+}
+plunderColection(["5", "40", "100"]);
+plunderColection(["10", "20", "380"]);
