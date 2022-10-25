cb30bfc18404b3a8ddfd627d35d0407a5079f464
georgi1141 <g.zh.yanev@gmail.com>
2022-10-25 21:00:03 +0200

    add exercise

---
+++ JS Fundamentals-September 2022/10.Objects and Classes/1.personInfo.js
@@ -0,0 +1,16 @@
+function info(firstName, lastName, age) {
+  let obj = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+  };
+
+  return obj
+}
+
+console.log(info("Peter", "Pan", "20"))
+
+// 1. Person Info.
+
+// Write a function that receives 3 parameters, sets them to an object, and returns that object.
+// The input comes as 3 separate strings in the following order: firstName, lastName, age
