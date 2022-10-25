70392f51b83ae9e4779960f09e243a5c179d8a64
georgi1141 <g.zh.yanev@gmail.com>
2022-10-25 21:58:48 +0200

    more exercise

---
+++ JS Fundamentals-September 2022/10.Objects and Classes/3.convertToObject.js
@@ -0,0 +1,17 @@
+function convert(data) {
+  let person = JSON.parse(data);
+
+  for (const key of Object.keys(person)) {
+    console.log(`${key}: ${person[key]}`);
+  }
+}
+
+convert('{"name": "George", "age": 40, "town": "Sofia"}');
+
+
+
+// 3. Convert to Object
+
+// Write a function that receives a string in JSON format and converts it to an object.
+
+// Loop through all the keys and print them with their values in format: "{key}: {value}"
