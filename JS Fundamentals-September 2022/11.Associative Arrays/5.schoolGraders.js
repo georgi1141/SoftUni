670dbb4b875f4064bcb449248840e4b34314c041
georgi1141 <g.zh.yanev@gmail.com>
2022-11-03 21:02:37 +0100

    added description

--- JS Fundamentals-September 2022/11.Associative Arrays/5.schoolGraders.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/5.schoolGraders.js
@@ -32,3 +32,9 @@ function calculation(data) {
 }
 
 calculation(["Lilly 4 6 6 5", "Tim 5 6", "Tammy 2 4 3", "Tim 6 6"]);
+
+
+// School Grades
+// Write a function that stores students and their grades throughout the year.If a student appears more than once, add the new grades to existing ones.Finally, print the students and their average grades, sorted alphabetically by student name.The input comes as an array of strings.
+
+//   Note: The average grades must be fixed to the second decimal place.
