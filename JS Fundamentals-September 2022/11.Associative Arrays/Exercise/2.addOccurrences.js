74e3895cf2533056c464e322b994041714abf447
georgi1141 <g.zh.yanev@gmail.com>
2022-11-04 16:37:51 +0100

    added exercise

--- JS Fundamentals-September 2022/11.Associative Arrays/Exercise/2.addOccurrences.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/Exercise/2.addOccurrences.js
@@ -1,19 +1,27 @@
 function extraction(data) {
+  let splitted = data.split(" ").map((x) => x.toLowerCase());
+  let Obj = {};
 
-  let splitted = data.split(' ')
-  let Obj = {}
+  splitted.forEach((element) => {
+    if (!Obj[element]) {
+      Obj[element] = 1;
+    } else {
+      Obj[element]++;
+    }
+  });
 
+  let entries = Object.entries(Obj).sort((a, b) => b[1] - a[1]);
+  let buffer = [];
+  for (const [key, value] of entries) {
+    if (value % 2 == 1) {
+      buffer.push(key);
+    }
+  }
 
-
-
-
-
+  console.log(buffer.join(" "));
 }
 
-
-extraction('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
-
-
+extraction("Java C# Php PHP Java PhP 3 C# 3 1 5 C#");
 
 // Odd Occurrences
 // Write a function that extracts the elements of a sentence, if it appears an odd number of times(case -insensitive).
