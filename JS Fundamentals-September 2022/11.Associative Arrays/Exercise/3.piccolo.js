99211cc9ff9b9833e8481683a456fec2f7dc2f8a
georgi1141 <g.zh.yanev@gmail.com>
2022-11-05 21:33:34 +0100

    desctiption added

--- JS Fundamentals-September 2022/11.Associative Arrays/Exercise/3.piccolo.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/Exercise/3.piccolo.js
@@ -35,3 +35,15 @@ solve([
   "IN, CA9876HH",
   "IN, CA2822UU",
 ]);
+
+
+
+// Piccolo
+// Write a function that:
+
+// • Records a car number for every car that enters the parking lot
+// • Removes a car number when the car goes out
+// • Input will be an array of strings in format[direction, carNumber]
+// Print the output with all car numbers which are in the parking lot sorted in ascending by number.
+
+// If the parking lot is empty, print: "Parking Lot is Empty".
