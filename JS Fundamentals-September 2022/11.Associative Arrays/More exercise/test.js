49837f32911d60fdada81b34b3d350039b516b55
georgi1141 <g.zh.yanev@gmail.com>
2022-11-06 21:00:02 +0100

    update

--- JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
@@ -1,13 +1,15 @@
 // get element between specific chars in string
 
+let someString = " user name [25] is cool";
 
-let someString = ' user name [25] is cool'
+let index1 = someString.indexOf("[");
+let index2 = someString.indexOf("]");
 
-let index1 = someString.indexOf('[')
-let index2 = someString.indexOf(']')
+let trimmed = someString.slice(index1 + 1, index2);
 
-let trimmed = someString.slice(index1 + 1, index2)
+console.log(index1, index2, trimmed);
 
-
-
-console.log(index1, index2, cutted);
+// destructuring an array without split method
+let arr = [1, 2, 3];
+let [x, y, c] = arr;
+console.log(x, y, c);
