0b3b5a709ad45e49cd0c985d94f58c9d6a62c3a4
georgi1141 <g.zh.yanev@gmail.com>
2022-11-06 17:59:13 +0100

    update

--- JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
@@ -1,9 +1,12 @@
+// get element between specific chars in string
+
+
 let someString = ' user name [25] is cool'
 
 let index1 = someString.indexOf('[')
 let index2 = someString.indexOf(']')
 
-let cutted = someString.slice(index1 + 1, index2)
+let trimmed = someString.slice(index1 + 1, index2)
 
 
 
