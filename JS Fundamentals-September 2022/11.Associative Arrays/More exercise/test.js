9a22d8e8105f092b3203927c010ad97d40556fc2
georgi1141 <g.zh.yanev@gmail.com>
2022-11-06 21:13:18 +0100

    testing some new modern javascript syntax

--- JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
+++ JS Fundamentals-September 2022/11.Associative Arrays/More exercise/test.js
@@ -13,3 +13,16 @@ console.log(index1, index2, trimmed);
 let arr = [1, 2, 3];
 let [x, y, c] = arr;
 console.log(x, y, c);
+
+// destructuring an Object
+
+let someObj = {
+  name: "gogo",
+  age: 5,
+};
+// with variable having same name as object key
+let { name } = someObj;
+//setting new name to variables instead of having the key object as a name
+let { age: myAge } = someObj;
+
+console.log(myAge, name);
