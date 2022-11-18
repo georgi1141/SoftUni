2bb277f054e002de8f1e2e5faaca0e91dd3ca2c5
georgi1141 <g.zh.yanev@gmail.com>
2022-11-18 13:16:48 +0100

    update

--- JS Fundamentals-September 2022/13.RegEx/3.matchDates.js
+++ JS Fundamentals-September 2022/13.RegEx/3.matchDates.js
@@ -3,8 +3,13 @@ function matchDates(input) {
   let pattern = /\b(?<days>\d{2})([-.\/])(?<month>[A-Z][a-z]{2,3})\2(?<year>\d{4})\b/g
   let matches = input.shift().matchAll(pattern)
 
+
   for (const match of matches) {
+    console.log(match);
+    //by the sequence of groups
     console.log(`Day: ${match[1]}, Month: ${match[3]}, Year: ${match[4]}`);
+    // by name of groups
+    console.log(`Day: ${match.groups.days}, Month: ${match.groups.month}, Year: ${match.groups.year}`);
   }
 }
 matchDates(['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016'])
