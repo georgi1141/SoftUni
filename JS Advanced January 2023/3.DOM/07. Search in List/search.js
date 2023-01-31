49dd9219aefe8aead9cbc214950206b2669ee940
georgi1141 <g.zh.yanev@gmail.com>
2023-01-31 16:34:26 +0200

    DOM exercise added

--- JS Advanced January 2023/3.DOM/07. Search in List/search.js
+++ JS Advanced January 2023/3.DOM/07. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const itemList = Array.from(document.querySelectorAll('#towns li'))
+   const resultDiv = document.querySelector('#result')
+   const searchInput = document.querySelector('#searchText')
+
+   let counter = 0
+
+   itemList.forEach(x => {
+      if (x.textContent.includes(searchInput.value)) {
+         x.style.fontWeight = 'bold'
+         x.style.textDecoration = 'underline'
+         counter++
+      } else {
+         x.style.fontWeight = 'normal'
+         x.style.textDecoration = 'none'
+      }
+   })
+   resultDiv.textContent = `${counter} matches found`
 }
