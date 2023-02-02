e8407d4cef6736cd53ec55aab33e6d3c3f591006
georgi1141 <g.zh.yanev@gmail.com>
2023-02-02 14:26:19 +0200

    exerceise added

--- JS Advanced January 2023/04.DOM Manipulations and Events/01. List of Items_Ресурси/03. Add-Delete/app.js
+++ JS Advanced January 2023/04.DOM Manipulations and Events/01. List of Items_Ресурси/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    // get reference to the user input's value
+    const input = document.getElementById("newItemText").value;
+    //create li element
+    const newLi = document.createElement("li");
+    // insert text to the new li element
+    newLi.textContent = input;
+    // check if there is an empty input
+    const list = document.getElementById("items");
+    //create delete btn
+    const deleteBtn = document.createElement("a");
+    deleteBtn.href = "#";
+    deleteBtn.textContent = "[delete]";
+    newLi.appendChild(deleteBtn);
+
+    //add finctionality to delete btn
+
+    deleteBtn.addEventListener("click", () => newLi.remove());
+
+    if (input !== "") {
+        //insert new li element into the list
+        list.appendChild(newLi);
+    }
+    // clear the input field after pressing add
+    document.getElementById("newItemText").value = "";
 }
