9d0d3eece289ae5d56133ef4f7f1b92ffd647615
georgi1141 <g.zh.yanev@gmail.com>
2023-02-02 15:10:26 +0200

    exercise added

--- JS Advanced January 2023/04.DOM Manipulations and Events/01. List of Items_Ресурси/04. Mouse-Gradient/app.js
+++ JS Advanced January 2023/04.DOM Manipulations and Events/01. List of Items_Ресурси/04. Mouse-Gradient/app.js
@@ -1,3 +1,10 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById("gradient");
+    gradient.addEventListener("mousemove", onClick);
+
+    function onClick(event) {
+        const x = event.offsetX;
+        const percent = Math.floor((x / 300) * 100);
+        document.getElementById("result").textContent = percent + "%";
+    }
 }
