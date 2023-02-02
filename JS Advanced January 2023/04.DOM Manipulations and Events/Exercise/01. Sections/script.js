4f4be1aeb2d2700cb69f849675c18fd8bb1130f3
georgi1141 <g.zh.yanev@gmail.com>
2023-02-02 22:35:09 +0200

    resolved problem

--- JS Advanced January 2023/04.DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ JS Advanced January 2023/04.DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   const parentDiv = document.querySelector("#content");
+
+   words.forEach((word) => {
+      const div = document.createElement("div");
+      const p = document.createElement("p");
+      p.textContent = word;
+      p.style.display = "none";
+      div.appendChild(p);
+      div.addEventListener("click", (event) => {
+         event.target.querySelector("p").style.display = "block";
+      });
+      parentDiv.appendChild(div);
+   });
 }
