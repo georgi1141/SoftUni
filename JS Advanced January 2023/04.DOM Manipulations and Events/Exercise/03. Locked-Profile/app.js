29b2ec063b5c5f104ee4abc2bb3b4506b57d7313
georgi1141 <g.zh.yanev@gmail.com>
2023-02-03 18:56:08 +0200

    solved problem

--- JS Advanced January 2023/04.DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JS Advanced January 2023/04.DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profiles = Array.from(document.querySelectorAll(".profile button"));
+
+    profiles.forEach((button) => button.addEventListener("click", onClick));
+
+    function onClick(event) {
+        const profile = event.target.parentElement;
+        let isActive = profile.querySelector('input[value="unlock"]').checked;
+
+        if (isActive) {
+            const info = profile.querySelector("div");
+
+            if (event.target.textContent === "Show more") {
+                info.style.display = "block";
+                profile.querySelector("button").textContent = "Hide it";
+            } else {
+                info.style.display = "none";
+                profile.querySelector("button").textContent = "Show more";
+            }
+        }
+    }
 }
