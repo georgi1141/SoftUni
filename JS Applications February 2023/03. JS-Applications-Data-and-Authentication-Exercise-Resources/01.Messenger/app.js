5033d8bb8c16339d73f68fac9ab6c89001f5e4e7
georgi1141 <g.zh.yanev@gmail.com>
2023-03-04 20:50:17 +0200

    exercise

--- JS Applications February 2023/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
+++ JS Applications February 2023/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
@@ -1,5 +1,41 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = "http://localhost:3030/jsonstore/messenger";
+  const messageElement = document.querySelector("#messages");
+  const sendBtn = document.querySelector("#submit");
+  const refreshBtn = document.querySelector("#refresh");
+  const authorElement = document.querySelector("#author");
+  const contentElement = document.querySelector("#content");
+
+  sendBtn.addEventListener("click", (e) => {
+    const data = {
+      author: authorElement.value,
+      content: contentElement.value,
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    authorElement.value = "";
+    contentElement.value = "";
+  });
+
+  refreshBtn.addEventListener("click", (e) => {
+  
+    
+    messageElement.value = "";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        Object.values(data).map(
+          (obj) => (messageElement.value += `${obj.author}: ${obj.content}\n`)
+        );
+      });
+  });
 }
 
 attachEvents();
