20b4800f65c3bc640f252f01cda3f007b54925bb
georgi1141 <g.zh.yanev@gmail.com>
2023-03-05 23:41:59 +0200

    exercise added

--- JS Applications February 2023/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ JS Applications February 2023/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,65 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const url = "http://localhost:3030/jsonstore/phonebook";
+
+  const loadBtn = document.querySelector("#btnLoad");
+  const createBtn = document.querySelector("#btnCreate");
+
+  const ulElement = document.querySelector("#phonebook");
+  const person = document.querySelector("#person");
+  const phone = document.querySelector("#phone");
+
+
+  loadBtn.addEventListener("click", () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        ulElement.innerHTML = "";
+        Object.entries(data).forEach((el) => {
+          const liElement = document.createElement("li");
+          liElement.textContent = `${el[1].person}: ${el[1].phone}`;
+          const deleteBtn = document.createElement("button",'Delete');
+          deleteBtn.setAttribute("id", "btnDelete");
+          deleteBtn.textContent = "Delete";
+          deleteBtn.style.display = "inline";
+
+          liElement.appendChild(deleteBtn);
+          ulElement.appendChild(liElement);
+
+          deleteBtn.addEventListener("click", (e) => {
+            
+            fetch(`${url}/${el[0]}`, {
+              method: "DELETE",
+              headers: {
+                "content-type": "application/json",
+              },
+            });
+
+            e.currentTarget.parentElement.remove();
+          });
+        });
+      });
+  });
+  createBtn.addEventListener("click", () => {
+    if(person.value !=='' && phone.value !== ''){
+        let data = {
+            person: person.value,
+            phone: phone.value,
+          };
+      
+          fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+                
+    }
+    
+    person.value = "";
+    phone.value = "";
+  });
 }
 
 attachEvents();
