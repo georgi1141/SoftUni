97a5d520faa5be856d85b5939d1e33d509b8ca5a
georgi1141 <g.zh.yanev@gmail.com>
2023-05-02 00:16:12 +0200

    update

--- JS Applications February 2023/Exam Prep 1/exam prep/Clear Career/src/views/offerDetailsView.js
+++ JS Applications February 2023/Exam Prep 1/exam prep/Clear Career/src/views/offerDetailsView.js
@@ -1,67 +1,48 @@
-
 import { html } from "../../node_modules/lit-html/lit-html.js";
 import { getOfferDetails, getUserData } from "../data/utility.js";
 
-const offerDetailsTemplate = (offer,isCreator) =>html`
-<section id="details">
-<div id="details-wrapper">
-  <img id="details-img" src=${offer.imageUrl} alt="example1" />
-  <p id="details-title">${offer.title}</p>
-  <p id="details-category">
-    Category: <span id="categories">${offer.category}</span>
-  </p>
-  <p id="details-salary">
-    Salary: <span id="salary-number">${offer.salary}</span>
-  </p>
-  <div id="info-wrapper">
-    <div id="details-description">
-      <h4>Description</h4>
-      <span
-        >${offer.description}</span
-      >
+const offerDetailsTemplate = (offer, isCreator) => html`
+  <section id="details">
+    <div id="details-wrapper">
+      <img id="details-img" src=${offer.imageUrl} alt="example1" />
+      <p id="details-title">${offer.title}</p>
+      <p id="details-category">
+        Category: <span id="categories">${offer.category}</span>
+      </p>
+      <p id="details-salary">
+        Salary: <span id="salary-number">${offer.salary}</span>
+      </p>
+      <div id="info-wrapper">
+        <div id="details-description">
+          <h4>Description</h4>
+          <span>${offer.description}</span>
+        </div>
+        <div id="details-requirements">
+          <h4>Requirements</h4>
+          <span>${offer.requirements}</span>
+        </div>
+      </div>
+      <p>Applications: <strong id="applications">1</strong></p>
+      ${isCreator
+        ? html` <div id="action-buttons">
+            <a href="/dashboard/${offer._id}/edit" id="edit-btn">Edit</a>
+            <a href="/dashboard/${offer._id}/delete" id="delete-btn">Delete</a>
+          </div>`
+        : null}
     </div>
-    <div id="details-requirements">
-      <h4>Requirements</h4>
-      <span
-        >${offer.requirements}</span
-      >
-    </div>
-  </div>
-  <p>Applications: <strong id="applications">1</strong></p>
-  ${isCreator ? html` <div id="action-buttons">
-              <a href="/dashboard/${offer._id}/edit" id="edit-btn">Edit</a>
-              <a href="/dashboard/${offer._id}/delete" id="delete-btn">Delete</a>`:null}
-`
+  </section>
+`;
 
 export const detailsView = async (ctx) => {
-    const id = ctx.params.id
+  const id = ctx.params.id;
 
-    const offerDetails = await getOfferDetails(id)
-    console.log(offerDetails);
-    let isCreator
-    const userData = getUserData()
-    if (userData){
-         isCreator = offerDetails._ownerId === userData._id
-    }
+  const offerDetails = await getOfferDetails(id);
+  console.log(offerDetails);
+  let isCreator;
+  const userData = getUserData();
+  if (userData) {
+    isCreator = offerDetails._ownerId === userData._id;
+  }
 
-
-
-
-
-  ctx.render(offerDetailsTemplate(offerDetails,isCreator));
-
-
+  ctx.render(offerDetailsTemplate(offerDetails, isCreator));
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
