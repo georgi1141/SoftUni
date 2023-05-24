2fa8f9c85a73f3e1d292bed8c3444025da61e90f
georgi1141 <g.zh.yanev@gmail.com>
2023-05-24 19:41:39 +0200

    update

--- JSWeb-May-2023/JSBack-End-May-2023/Intro-to-Node.js/cat-shalter-with-espressJS/index.js
+++ JSWeb-May-2023/JSBack-End-May-2023/Intro-to-Node.js/cat-shalter-with-espressJS/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const fs = require("fs");
-const app = express();
 const breeds = require("./breed.js");
+const app = express();
 
 // setting the view engine
 app.engine("handlebars", handlebars.engine());
@@ -12,6 +12,7 @@ const bodyParser = express.urlencoded({ extended: false });
 app.use(bodyParser);
 app.use(express.static("public"));
 
+// App views
 app.get("/", (req, res) => {
   fs.readFile("./cats.json", "utf8", (err, data) => {
     if (err) throw err;
@@ -29,6 +30,66 @@ app.post("/addBreed", (req, res) => {
 });
 app.get("/addCat", (req, res) => {
   res.render("addCat", { breed: breeds });
+});
+app.post("/addCat", (req, res) => {
+  const id = Math.floor(Math.random() * 1000000) + 1;
+  const name = req.body.name;
+  const description = req.body.description;
+  const image = req.body.image;
+  const breed = req.body.breed;
+
+  const data = { id, name, description, image, breed };
+
+  fs.readFile("./cats.json", "utf8", (err, cats) => {
+    if (err) throw err;
+    cats = JSON.parse(cats);
+    cats.push(data);
+    fs.writeFile("./cats.json", JSON.stringify(cats), (err, data) => {
+      if (err) throw err;
+      res.redirect("/");
+      res.end();
+    });
+  });
+});
+app.get("/deleteCat/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  fs.readFile("./cats.json", "utf8", (err, cats) => {
+    if (err) throw err;
+    const data = JSON.parse(cats);
+    let result = [];
+    data.forEach((cat) => {
+      if (cat.id !== id) result.push(cat);
+    });
+    fs.writeFile("./cats.json", JSON.stringify(result), (err) => {
+      if (err) throw err;
+    });
+  });
+  res.redirect("/");
+  res.end();
+});
+app.get("/editCat/:id", (req, res) => {
+
+    res.render('editCat')
+
+//   const id = Number(req.params.id);
+
+//   const name = req.body.name;
+//   const description = req.body.description;
+//   const breed = req.body.breed;
+
+//   fs.readFile("./cats.json", "utf8", (err, data) => {
+//     if (err) throw err;
+
+//     data.forEach((cat) => {
+//       if (cat.id !== id) {
+//         cat.id = id;
+//         cat.description = description;
+//         cat.breed = breed;
+//         cat.name = name;
+//       }
+//     });
+//   });
 });
 
 app.listen(5000, () => console.log("listening on port 5000"));
