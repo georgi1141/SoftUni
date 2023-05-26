761e5949b2a336d0559c212250f0e7b0ff361b30
georgi1141 <g.zh.yanev@gmail.com>
2023-05-26 20:15:04 +0200

    extract common html to main layout

--- JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/index.js
+++ JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const port = 3000
 const app = express();
+
+// setting up handlebars template engine
+app.engine('hbs',handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine','hbs')
+app.set('views','src/views')
+
 
 app.get('/', (req,res)=>{
 
 
-    res.send('hello world')
+    res.render('index')
 });
 
 
