0b0b5c508b1aad022eb08a2d955f73a106f85003
georgi1141 <g.zh.yanev@gmail.com>
2023-05-28 18:00:00 +0200

    adding cube details page

--- JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/controllers/cubeController.js
+++ JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/controllers/cubeController.js
@@ -1,24 +1,21 @@
-const router = require('express').Router()
-const cubeService = require('../services/cubeService')
+const router = require("express").Router();
+const cubeService = require("../services/cubeService");
 
+router.get("/create", (req, res) => {
+  res.render("create");
+});
+router.post("/create", (req, res) => {
+  const { name, description, imageUrl, difficultyLevel } = req.body;
 
-router.get('/create',(req,res)=>{
+  cubeService.createCube({ name, description, imageUrl, difficultyLevel });
 
-
-    res.render('create')
-})
-router.post('/create',(req,res)=>{
-
-    const {name,description,imageUrl,difficultyLevel} = req.body
-
-    cubeService.createCube({name,description,imageUrl,difficultyLevel})
-
-
-    res.redirect('/')
-
+  res.redirect("/");
 });
 
+router.get('/:cubeID/details', (req, res) =>{
+    const id = req.params.cubeID
+    const cube = cubeService.getOneCube(id)
+    res.render('details',{cube})
+  })
 
-
-
-module.exports = router
+module.exports = router;
--- JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/services/cubeService.js
+++ JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/services/cubeService.js
@@ -3,6 +3,8 @@ const uniqid = require('uniqid')
 
 exports.getAll = ()=>cubes.slice()
 
+exports.getOneCube = (id)=> cubes.find(x=>x.id == id)
+
 exports.createCube = (cubeData)=>{
 const newCube = {
     id:uniqid(),
