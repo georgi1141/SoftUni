eef4c61cb9cf107a14faee8199db10e9e49fe1bd
georgi1141 <g.zh.yanev@gmail.com>
2023-06-06 23:28:35 +0200

    add create user model and validate password

--- JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/controllers/userController.js
+++ JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/controllers/userController.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
+const userService = require('../services/userService')
 
 
 
 router.get('/register',(req,res)=>{
     res.render('auth/registerPage')
+})
+router.post('/register',async (req,res)=>{
+    const {username,password,repeatPassword} = req.body
+    console.log(password,repeatPassword)
+
+    await userService.register({username,password,repeatPassword} )
+
+
+    res.redirect('/user/login')
+
 })
 
 router.get('/login',(req ,res)=>{
---
+++ JSWeb-May-2023/JSBack-End-May-2023/ExpressJS-and-Templatig/src/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:{
+        type:String
+    }
+})
+
+userSchema.virtual('repeatPassword')
+.set(function(value){
+    if(value!== this.password){
+        throw new mongoose.MongooseError('Passwords do not match!')
+
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = User
