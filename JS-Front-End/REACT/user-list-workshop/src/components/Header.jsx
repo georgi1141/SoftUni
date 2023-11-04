70100bf5777fce02e9621f34936a2e7a3387db84
georgi1141 <g.zh.yanev@gmail.com>
2023-11-04 12:23:14 +0100

    UserList exercise /devide into components

---
+++ JS-Front-End/REACT/user-list-workshop/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+    <div className="logo">
+      <span className="course">React Workshop - September 2023</span>
+    </div>
+  </header>
+  )
+}
+
+export default Header
